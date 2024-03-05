2125023dd91fa7340e00257256b834c12fb62f8e
Md Rashed Ali <tools.rashed@gmail.com>
2024-03-05 14:21:21 +0600

    add /blog banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import BlogPost from "@/components/BlogPost";
 import NavigationBar from "@/components/NavigationBar";
-import Image from "next/image";
+import BlogBanner from "@/components/BlogBanner";
 
 export default function Home() {
   return (
@@ -13,12 +13,15 @@ export default function Home() {
           className="bg-gradient-to-br from-slate-50 via-slate-50 to-[#f9daff] h-[100vh]"
         ></div>
 
-        {/* BLOGS CONTAINER */}
+        {/* BLOGS SECTION */}
         <section className="max-w-[1290px] mx-auto my-20">
           <div className="blogs grid grid-cols-2 gap-10">
             <BlogPost />
             <BlogPost />
           </div>
+
+          {/* BLOG BANNER */}
+        <BlogBanner/>
         </section>
       </main>
     </div>
---
+++ src/components/BlogBanner.tsx
@@ -0,0 +1,19 @@
+const BlogBanner = () => {
+  return (
+    <div>
+        <div className="bg-[#20172d] py-24 my-12" style={{backgroundImage: `url('https://i.ibb.co/17HtQ73/blog-banner-bg.jpg')`, backgroundBlendMode: "color-dodge", backgroundPosition: "center", backgroundRepeat: "no-repeat", backgroundSize: "cover"}}>
+            <div className="content p-12 bg-white/10 backdrop-blur-lg w-[60%] mx-auto text-center space-y-5">
+              <h1 className="text-white text-3xl">Creative team, intuitive solution</h1>
+              <p className="text-white/80">
+                Lorem ipsum dolor sit amet consectetur. Enim vitae hendrerit
+                consectetur cras in nulla. Non suspendisse at ac enim. Facilisi
+                nulla tortor nec mollis faucibus odio sodales.
+              </p>
+              <button className="bg-white px-3 py-2">Let&apos;s Talk</button>
+            </div>
+          </div>
+    </div>
+  )
+}
+
+export default BlogBanner
