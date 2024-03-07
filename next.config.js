aa96d38edc0a3a5e29121f955a8bc3443803bd74
Md Rashed Ali <tools.rashed@gmail.com>
2024-03-08 00:40:41 +0600

    config updated

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "export",
-    images: {
-        unoptimized: true
-    },
+    basePath: "/abcandy",
+    reactStrictMode: true,
+
   // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
   // trailingSlash: true,
  
