22e722e636066d73d3eef4bc4ef3ce00d782a42f
Md Rashed Ali <tools.rashed@gmail.com>
2024-04-08 09:16:54 +0600

    fix /slider effect

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -6,6 +6,8 @@ import { FaArrowLeftLong, FaArrowRight } from "react-icons/fa6";
 
 export const Carousel = () => {
   const [slide, setSlide] = React.useState(2);
+  const [dragStartX, setDragStartX] = useState(null);
+  const containerRef = useRef(null);
   const rawData = getJsonData();
   const data = rawData.filter((singleData) =>
     singleData.tags.includes("carousel")
@@ -17,74 +19,86 @@ export const Carousel = () => {
 
   const prevSlide = () => {
     setSlide(slide === 0 ? data.length - 1 : slide - 1);
-
+  };
+  const handleDragStart = (e: any) => {
+    setDragStartX(e.clientX);
   };
 
-
-
-  const handleDrag = (event:any) => {
-console.log(event.clientX)
-    if(event.clientX > 600){
-      nextSlide()
-      return
-    }
-
-
-    if(event.clientX < 100){
+  const handleDragMove = (e: any) => {
+    if (dragStartX === null) return;
+    const deltaX = e.clientX - dragStartX;
+    // Adjust the threshold as needed
+    if (deltaX > 10) {
+      // Move to the previous slide
       prevSlide()
-      return
+    } else if (deltaX < -10) {
+      // Move to the next slide
+      nextSlide()
     }
-   
-  }
+  };
+
+  const handleDragEnd = () => {
+    setDragStartX(null);
+  };
+
   return (
     <div className="font-dm carousel relative w-full flex justify-center items-center">
       <div className="relative w-full flex justify-center items-center overflow-hidden">
         <div
-        
-          className="flex justify-center  gap-[30px] translate-x-[345px] md:translate-x-[530px] lg:translate-x-[830px] lg:h-[650px] "
-          style={{ transition: "opacity 0.0s"}}
+          className="flex justify-center  gap-[30px] translate-x-[345px] md:translate-x-[530px] lg:translate-x-[830px] lg:h-[650px] select-none"
+          style={{ transition: "opacity 0.0s" }}
         >
           {[...data, ...data, ...data].map((item, idx) => {
             const adjustedIdx = idx % data.length;
             return (
               <div
-             
-              onDrag={handleDrag}
-            
+                className="slide-container"
+                ref={containerRef}
+                onMouseDown={handleDragStart}
+                onMouseMove={handleDragMove}
+                onMouseUp={handleDragEnd}
+                onMouseLeave={handleDragEnd}
                 key={idx}
                 style={{
                   transform:
-                slide === 0
-                  ? "translateX(0.5%)"
-                  : slide === 1
-                  ? "translateX(-103.3%)"
-                  : slide === 2
-                  ? "translateX(-207%)"
-                  : "",
-                  transition: `opacity 0.8s ${slide === adjustedIdx ? "ease" : "none"}`,
-
+                    slide === 0
+                      ? "translateX(0.5%)"
+                      : slide === 1
+                      ? "translateX(-103.3%)"
+                      : slide === 2
+                      ? "translateX(-207%)"
+                      : "",
+                  transition: ` ${
+                    slide === adjustedIdx ? "opacity 0.8s ease" : "none"
+                  }`,
                 }}
                 className={`slide cursor-move w-[350px] md:w-[500px] lg:w-[800px] relative ${
-                  adjustedIdx === slide ? "z-0" : "opacity-0 md:opacity-40 -z-50"
+                  adjustedIdx === slide
+                    ? "z-0"
+                    : "opacity-0 md:opacity-40 -z-50"
                 }`}
               >
                 <div className="w-[350px] md:w-[500px] lg:w-[800px] h-[250px] md:h-[300px] lg:h-[400px]">
-                <Image
-                  src={
-                    Array.isArray(item.imageSrc)
-                      ? item.imageSrc[0]
-                      : item.imageSrc || ""
-                  }
-                  alt={item.title}
-                  width={350}
-                  height={400}
-                  className="object-cover w-full h-full"
-                />
+                  <Image
+                    src={
+                      Array.isArray(item.imageSrc)
+                        ? item.imageSrc[0]
+                        : item.imageSrc || ""
+                    }
+                    alt={item.title}
+                    width={350}
+                    height={400}
+                    draggable={true}
+                    className="object-cover w-full h-full"
+                  />
                 </div>
-                
+
                 <div>
                   {adjustedIdx === slide && (
-                    <div className="card-body bg-white px-5  py-4  text-text-primary">
+                    <div
+                      draggable={true}
+                      className="card-body bg-white px-5  py-4  text-text-primary"
+                    >
                       <div className="publish-meta flex gap-3 text-sm">
                         <p className="category font-medium uppercase">
                           {item?.category}
@@ -92,7 +106,9 @@ console.log(event.clientX)
                         <p className="Date !text-text-muted">{item?.date}</p>
                       </div>
                       <div className="content  pt-4 space-y-2">
-                        <h1 className="text-2xl font-lora font-medium">{item?.title}</h1>
+                        <h1 className="text-2xl font-lora font-medium">
+                          {item?.title}
+                        </h1>
                         <p className="!text-text-muted font-dm text-base hidden lg:block">
                           {item?.content}
                         </p>
@@ -108,23 +124,22 @@ console.log(event.clientX)
                   )}
                 </div>
                 {/* PREV AND NEXT BUTTONS */}
-                {
-                  adjustedIdx === slide &&  <div className="hidden lg:flex absolute z-50 top-0 justify-between w-[490px] md:w-[640px] lg:w-[940px] -translate-x-[70px] translate-y-36">
-                  <button
-                    onClick={prevSlide}
-                    className="bg-text-primary p-3 text-white"
-                  >
-                    <FaArrowLeftLong className="arrow arrow-left" />
-                  </button>
-                  <button
-                    onClick={nextSlide}
-                    className="bg-text-primary p-3 text-white"
-                  >
-                    <FaArrowRight className="arrow arrow-right" />
-                  </button>
-                </div>
-                }
-               
+                {adjustedIdx === slide && (
+                  <div className="hidden lg:flex absolute z-50 top-0 justify-between w-[490px] md:w-[640px] lg:w-[940px] -translate-x-[70px] translate-y-36">
+                    <button
+                      onClick={prevSlide}
+                      className="bg-text-primary p-3 text-white"
+                    >
+                      <FaArrowLeftLong className="arrow arrow-left" />
+                    </button>
+                    <button
+                      onClick={nextSlide}
+                      className="bg-text-primary p-3 text-white"
+                    >
+                      <FaArrowRight className="arrow arrow-right" />
+                    </button>
+                  </div>
+                )}
               </div>
             );
           })}
@@ -152,4 +167,3 @@ console.log(event.clientX)
     </div>
   );
 };
-
